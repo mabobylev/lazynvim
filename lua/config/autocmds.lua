60e7ce7f7d4506805da5a0987eae8908cc4ed3b7
Mihail Bobylev <mabobylev@gmail.com>
2024-05-24 10:31:34 +0400

    Update autocmds.lua

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,3 +1,38 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+
+-- This file is automatically loaded by lazyvim.config.init.
+
+local function augroup(name)
+  return vim.api.nvim_create_augroup("lazyvim_" .. name, { clear = true })
+end
+
+-- Wrap and check for spell in text filetypes
+vim.api.nvim_create_autocmd("FileType", {
+  group = augroup("wrap_spell"),
+  pattern = { "gitcommit", "markdown" },
+  callback = function()
+    vim.opt_local.wrap = true
+    vim.opt_local.spelllang = "en_us,ru_ru"
+    vim.opt_local.spell = true
+  end,
+})
+
+-- Auto create dir when saving a file, in case some intermediate directory does not exist
+vim.api.nvim_create_autocmd({ "BufWritePre" }, {
+  group = augroup("auto_create_dir"),
+  callback = function(event)
+    if event.match:match("^%w%w+:[\\/][\\/]") then
+      return
+    end
+    local file = vim.uv.fs_realpath(event.match) or event.match
+    vim.fn.mkdir(vim.fn.fnamemodify(file, ":p:h"), "p")
+  end,
+})
+
+-- Autocreate file from template
+vim.cmd("autocmd BufNewFile  *.c   0r ~/Templates/template.c")
+vim.cmd("autocmd BufNewFile  *.h   0r ~/Templates/template.h")
+vim.cmd("autocmd BufNewFile  *.sh   0r ~/Templates/template.sh")
+vim.cmd("autocmd BufNewFile  *.py   0r ~/Templates/template.py")
