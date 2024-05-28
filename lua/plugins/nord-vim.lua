539232747f2c60cfc8a74a4d6aceababc0d8e5ad
Mihail Bobylev <mabobylev@gmail.com>
2024-05-28 13:02:57 +0400

    Add autocommands and nord colorscheme

---
+++ lua/plugins/nord-vim.lua
@@ -0,0 +1,12 @@
+return {
+  -- add nord colorscheme
+  { "arcticicestudio/nord-vim" },
+
+  -- Configure LazyVim to load gruvbox
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "nord",
+    },
+  },
+}
