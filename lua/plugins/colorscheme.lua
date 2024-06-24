93ba4b88374a199dc3b5d49760aa7dd277ca2767
Mihail Bobylev <mabobylev@gmail.com>
2024-06-24 19:24:27 +0400

    Update

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,14 @@
+return {
+  -- add nord colorscheme
+  -- { "shaunsingh/nord.nvim" },
+
+  -- Configure LazyVim to load nord colorscheme
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      -- colorscheme = "nord",
+      colorscheme = "catppuccin-frappe",
+      -- flavour = "frappe", -- latte, frappe, macchiato, mocha, auto
+    },
+  },
+}
